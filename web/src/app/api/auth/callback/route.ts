85d26dd3208ba3f332abb8a7c4cfa6b5feb96a29
Raphael <raphaelheineck@gmail.com>
2023-06-07 15:41:05 -0300

    Aula 04

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -5,19 +5,21 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
 
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
   const registerResponse = await api.post('/register', {
     code,
   })
 
   const { token } = registerResponse.data
 
-  const redirectURL = new URL('/', request.url)
+  const redirectURL = redirectTo ?? new URL('/', request.url)
 
   const cookieExpiresInSeconds = 60 * 60 * 24 * 30
 
   return NextResponse.redirect(redirectURL, {
     headers: {
-      'Set-cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}`,
+      'Set-cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
     },
   })
 }
